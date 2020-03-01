932eb7e6573a88095f949793ea27b1118228dd41
cgenel <genel661@gmail.com>
2020-02-29 22:09:16 -0800

    added scraping tools

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 // dependencies
 var express = require("express");
 var mongoose = require("mongoose");
-var path = require("path");
 var expressHandlebars = require("express-handlebars");
+
+// Our scraping tools
+// Axios is a promised-based http library, similar to jQuery's Ajax method
+// It works on the client and on the server
+var axios = require("axios");
+var cheerio = require("cheerio");
 
 // setup port
 var PORT = process.env.PORT || 3000;
@@ -17,6 +22,7 @@ var router = express.Router();
 app.use(express.static(__dirname + "/public"));
 
 app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
 
 // have requests go through router middleware
 app.use(router);
@@ -25,14 +31,7 @@ app.use(router);
 var db = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
 // connect mongoose to database
-mongoose.connect(db, function(error) {
-  if(error) {
-    console.log(error);
-  }
-  else {
-    console.log("mongoose connection is successful");
-  }
-});
+mongoose.connect("mongodb://localhost/unit18Populater", { useUnifiedTopology: true, useNewUrlParser: true });
 
 // listen on port 3000
 app.listen(PORT, function() {
