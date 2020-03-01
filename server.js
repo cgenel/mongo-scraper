3565023fe4e65a46bab2be04895f5f0ae3e929f6
cgenel <genel661@gmail.com>
2020-02-29 21:36:45 -0800

    added mongoose

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // dependencies
 var express = require("express");
+var mongoose = require("mongoose");
 var path = require("path");
 var expressHandlebars = require("express-handlebars");
 
@@ -19,6 +20,19 @@ app.use(express.urlencoded({ extended: true }))
 
 // have requests go through router middleware
 app.use(router);
+
+// use deployed database, if not deployed use local database
+var db = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+// connect mongoose to database
+mongoose.connect(db, function(error) {
+  if(error) {
+    console.log(error);
+  }
+  else {
+    console.log("mongoose connection is successful");
+  }
+});
 
 // listen on port 3000
 app.listen(PORT, function() {
