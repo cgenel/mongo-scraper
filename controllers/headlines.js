7561a7b06afb61103d787829fb1b099ef34d3024
cgenel <genel661@gmail.com>
2020-03-02 15:43:14 -0800

    fetch and scrape new articles, functions to update articles complete

---
+++ controllers/headlines.js
@@ -0,0 +1,49 @@
+// bring in scrape script and makeDate scripts
+var scrape = require("../scripts/scrape");
+var makeDate = require("../scripts/date");
+
+// bring in headline and note models
+var Headline = require("../models/Headline");
+
+// functionality for deleting and saving articles
+module.exports = {
+
+  // fetch will run the scrape function to grab all articles and insert into headline collection in mongoDB
+  fetch: function(cb) {
+    // run scrape, set data to be called 'articles'
+    scrape(function(data) {
+      var articles = data;
+
+      // loop through each article and run makeDate function
+      for (var i = 0; i < articles.length; i++) {
+        articles[i].date = makeDate();
+        articles[i].saved = false;
+      }
+
+      // mongo function to grab headline and insert many articles into the collection, (err, docs) will skip over errors 
+      Headline.collections.insertMany(articles, {ordered:false}, function(err, docs) {
+        cb(err, docs);
+      });
+    });
+  },
+
+  // delete propert to remove articles
+  delete: function(query, cb) {
+    Headline.remove(query, cb);
+  },
+
+  // get all items out with 'get' function and sort items from most recent to least recent
+  get: function(query, cb) {
+    Headline.find(query).sort({
+      _id: -1
+    })
+    .exec(function(err, doc) {
+      cb(doc);
+    });
+  },
+
+  // function to update new articles that are scraped with relevant id and any information
+  update: function(err, cb) {
+    Headline.update({_id: query._id}, {$set: query}, {}, cb);
+  }
+}
