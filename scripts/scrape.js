f9bcfb122ec092dc5c3ec816eaef58ccd5d71311
cgenel <genel661@gmail.com>
2020-03-07 17:23:30 -0800

    scrapin wall street journal success, app working

--- scripts/scrape.js
+++ scripts/scrape.js
@@ -1,37 +1,48 @@
-// Our scraping tools
+// scraping tools
 // Axios is a promised-based http library, similar to jQuery's Ajax method
 // It works on the client and on the server
 var request = require("request");
 var cheerio = require("cheerio");
 
+// scrape function (cb) is "callback"
 var scrape = function (cb) {
 
-  // requesting from new york times
-  request("http://www.nytimes.com", function(err, res, bodyy) {
+  // requesting from wsj.com(wall street journal)
+  request("https://www.wsj.com/", function(err, res, body) {
 
+    // cheerio.load in body with '$' as selector to be used like jquery
      var $ = cheerio.load(body);
 
-    //  empty articles array
+    //  new empty articles array
      var articles = [];
 
-     $(".theme-summary").each(function(i, element) {
+    // select all theme--storys from new york times
+     $(".WSJTheme--story--pKzwqDTt").each(function(i, element) {
 
-      var head = $(this).children(".story-heading").text().trim();
-      var sum = $(this).children(".summary").text().trim();
+      // grab text for story headline & theme summary and trim off white space set to variables head & sum
+      var head = $(this).children(".WSJTheme--summary--12br5Svc").text().trim();
+      var sum = $(this).children(".WSJTheme--summary--12br5Svc").text().trim();
 
+      // if scrape success, then use replace regExp methods, cleans up text with white space
       if(head && sum) {
         var headNeat = head.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
         var sumNeat = sum.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
 
+        // take the data from headNeat & sumNeat and use as headline and summary for new articles
         var dataToAdd = {
           headline: headNeat,
           summary: sumNeat
         };
 
+        // push new data to articles array
         articles.push(dataToAdd);
       }
      });
+
+    //  call back function to send articles
      cb(articles);
   });
 };
+
+// export scrape
 module.exports = scrape;
